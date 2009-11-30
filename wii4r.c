e970c1be4f44d5fb7ba66828501c69940d05c7d5
Mattia Bruno <mrbrown2@tele2.it>
2009-12-01 00:22:51 +0100

    wii4r.c updated: added all expansion functions

--- wii4r.c
+++ wii4r.c
@@ -267,6 +267,22 @@ static VALUE rb_wm_just_pressed(VALUE self, VALUE arg) {
   else return Qtrue;
 }
 
+static VALUE rb_wm_held(VALUE self, VALUE arg) {
+  wiimote *wm;
+  Data_Get_Struct(self, wiimote, wm);
+  int held = IS_HELD(wm, NUM2INT(arg));
+  if(held == 0) return Qfalse;
+  else return Qtrue;
+}
+
+static VALUE rb_wm_released(VALUE self, VALUE arg) {
+  wiimote *wm;
+  Data_Get_Struct(self, wiimote, wm);
+  int released = IS_RELEASED(wm, NUM2INT(arg));
+  if(released == 0) return Qfalse;
+  else return Qtrue;
+}
+
 static VALUE rb_wm_yaw(VALUE self) {
   wiimote *wm;
   Data_Get_Struct(self, wiimote, wm);
@@ -527,10 +543,10 @@ static VALUE rb_cm_cp(VALUE self) {
 static VALUE rb_wm_led(VALUE self) {
   wiimote *wm;
   Data_Get_Struct(self, wiimote, wm);
-  if(WIIUSE_IS_LED_SET(wm, WIIMOTE_LED_1)) return rb_str_new2("LED_1");
-  if(WIIUSE_IS_LED_SET(wm, WIIMOTE_LED_2)) return rb_str_new2("LED_2");
-  if(WIIUSE_IS_LED_SET(wm, WIIMOTE_LED_3)) return rb_str_new2("LED_3");
-  if(WIIUSE_IS_LED_SET(wm, WIIMOTE_LED_4)) return rb_str_new2("LED_4");
+  if(WIIUSE_IS_LED_SET(wm, 1)) return rb_str_new2("LED_1");
+  if(WIIUSE_IS_LED_SET(wm, 2)) return rb_str_new2("LED_2");
+  if(WIIUSE_IS_LED_SET(wm, 3)) return rb_str_new2("LED_3");
+  if(WIIUSE_IS_LED_SET(wm, 4)) return rb_str_new2("LED_4");
   return Qnil;
 }
 
@@ -560,6 +576,27 @@ static VALUE rb_wm_exp(int argc, VALUE * argv, VALUE self) {
         else return Qfalse;
     }
   }
+}
+
+static VALUE rb_wm_nunchuk(VALUE self) {
+  wiimote *wm;
+  Data_Get_Struct(self, wiimote, wm);
+  if(wm->exp.type == EXP_NUNCHUK) return Qtrue;
+  else return Qfalse;
+}
+
+static VALUE rb_wm_cc(VALUE self) {
+  wiimote *wm;
+  Data_Get_Struct(self, wiimote, wm);
+  if(wm->exp.type == EXP_CLASSIC) return Qtrue;
+  else return Qfalse;
+}
+
+static VALUE rb_wm_gh(VALUE self) {
+  wiimote *wm;
+  Data_Get_Struct(self, wiimote, wm);
+  if(wm->exp.type == EXP_GUITAR_HERO_3) return Qtrue;
+  else return Qfalse;
 }
 
 void Init_wii4r() {
@@ -645,18 +682,13 @@ void Init_wii4r() {
   rb_define_method(wii_class, "status", rb_wm_status, 0);
   rb_define_method(wii_class, "connected?", rb_wm_connected, 0);
   rb_define_method(wii_class, "expansion?", rb_wm_exp, -1);
-  //
-  //rb_define_method(wii_class, "nunchuk?", rb_wm_nunchuk, 0);
-  //
-  //rb_define_method(wii_class, "classic_controller?", rb_wm_cc, 0);
-  //
-  //rb_define_method(wii_class, "guitar_hero_controller?", rb_wm_gh, 0);
+  rb_define_method(wii_class, "has_nunchuk?", rb_wm_nunchuk, 0);
+  rb_define_method(wii_class, "has_classic_controller?", rb_wm_cc, 0);
+  rb_define_method(wii_class, "has_guitar_hero_controller?", rb_wm_gh, 0);
   rb_define_method(wii_class, "pressed?", rb_wm_pressed, 1);
   rb_define_method(wii_class, "just_pressed?", rb_wm_just_pressed, 1);
-  //
-  //rb_define_method(wii_class, "held?", rb_wm_held, 1);
-  //
-  //rb_define_method(wii_class, "released?", rb_wm_released, 1);
+  rb_define_method(wii_class, "held?", rb_wm_held, 1);
+  rb_define_method(wii_class, "released?", rb_wm_released, 1);
   rb_define_alias(wii_class, "using_accelerometer?", "motion_sensing?");
   rb_define_method(wii_class, "roll", rb_wm_roll, 0);
   rb_define_method(wii_class, "absolute_roll", rb_wm_aroll, 0);
