535d6a969fa5ec4dde0c56635358be344bb789f5
Mattia Bruno <mrbrown2@tele2.it>
2009-12-18 21:15:09 +0100

    started documentation task

--- wii4r.h
+++ wii4r.h
@@ -1,35 +1,30 @@
 #ifndef WII4R_H
 #define WII4R_H
 
-#ifndef WIN32
-	#include<unistd.h>
-#endif
-
-#ifndef WIIMOTE_IS_SET
-#define WIIMOTE_IS_SET(wm, s)			((wm->state & (s)) == (s))
-#endif
-
-#ifndef WIIMOTE_IS_CONNECTED
-#define WIIMOTE_IS_CONNECTED(wm)		(WIIMOTE_IS_SET(wm, 0x0008))
-#endif
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<wiiuse.h>
 #include<ruby.h>
 
+//Wii module
 extern VALUE wii_mod;
+
+//WiimoteManager class
 extern VALUE cm_class;
+
+//Wiimote class
 extern VALUE wii_class;
 
+//disconnect a wiimote when a Wiimote object is marked by the GC and must be deleted
 extern void free_wiimote(void * wm);
+
+//return true if the wiimote "wm" is connected, false otherwise
 extern int wm_connected(wiimote *wm);
 
-
+//struct to describe the WiimoteManager class
 typedef struct _connman {
-  wiimote **wms;
-  int n;
+  wiimote **wms;		//array of ptrs to wiimote structures
+  int n;			//max number of wiimotes connected
 } connman;
 
-#endif
+#endif //WII4R_H
