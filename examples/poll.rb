d672940f14d98b384dd4c5a80a2477166e2df84b
Giorgio <buzz@buzz-laptop.(none)>
2009-12-19 03:48:47 +0100

    added GPL license

--- examples/poll.rb
+++ examples/poll.rb
@@ -1,6 +1,26 @@
-require 'wii4r'
+############################################################################
+# 																		   #
+# Copyright (C) 2009 by KzMz KzMz@modusbibendi.org                         #
+# Copyright (C) 2009 by BuZz Gambino.Giorgio@gmail.com                     #
+#                                                                          #
+# This program is free software; you can redistribute it and/or modify     #
+# it under the terms of the GNU General Public License as published by     #
+# the Free Software Foundation; either version 2 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# This program is distributed in the hope that it will be useful,          #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             #
+# GNU General Public License for more details.                             #
+#                                                                          #
+# You should have received a copy of the GNU General Public License        #
+# along with this program; if not, write to the                            #
+# Free Software Foundation, Inc.,                                          #
+# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.                 #
+############################################################################
 
-w = Wii::WiimoteManager.new
+require 'wii4r'
+include Wii
 
 def set_rumble(w)
 	w.rumble= w.rumble? ?  false : true 
@@ -43,8 +63,9 @@ def speaker(w)
 end
 
 begin
-
-
+   
+  w = WiimoteManager.new
+  
   puts "Connecting.... Press 1 and 2 BUTTONS"
   
   w.connect
@@ -80,6 +101,3 @@ end
 ensure
     w.cleanup!
 end
-
-
-
