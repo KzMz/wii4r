cc6e950220f7ba759133b78df6c4f2e584126561
Giorgio <buzz@buzz-laptop.(none)>
2009-12-19 02:16:36 +0100

    modified poll.rb

--- poll.rb
+++ poll.rb
@@ -2,7 +2,48 @@ require 'wii4r'
 
 w = Wii::WiimoteManager.new
 
+def set_rumble(w)
+	w.rumble= w.rumble? ?  false : true 
+end
+
+def status(w)
+	puts w.status
+end
+
+def accel_on(w)
+    w.motion_sensing = true
+	puts "Pitch: #{w.pitch}"
+	puts "Roll:  #{w.roll}"
+	w.motion_sensing = false
+end
+
+def vres(w)
+	puts "Virtual Resolution => #{w.virtual_resolution}"
+end
+
+def sens_pos(w)
+	puts "Sensor Bar Position => #{w.sensor_bar_position}"
+end
+
+def a_ratio(w)
+	puts "Aspect Ratio => #{w.aspect_ratio}"
+end
+
+def sens(w)
+	w.ir = true
+	puts "IR Sensitivity Level => #{w.sensitivity}"
+end
+
+def speaker(w)
+
+	if w.speaker? 
+	puts "Wiimote is using speaker"
+	else puts "Wiimote is not using speaker"
+	end
+end
+
 begin
+
 
   puts "Connecting.... Press 1 and 2 BUTTONS"
   
@@ -12,25 +53,25 @@ begin
     w.poll do |(wm, event)|
       if event == :generic
         if wm.pressed? Wii::BUTTON_A
-			puts "A!"
-        elsif wm.pressed? Wii::BUTTON_B
-            puts "B!"
+			set_rumble(wm)
+		elsif wm.pressed? Wii::BUTTON_B
+            status(wm)
         elsif wm.pressed? Wii::BUTTON_DOWN
-            puts "DOWN!"
+            speaker(wm)
         elsif wm.pressed? Wii::BUTTON_LEFT
             puts "LEFT!"
         elsif wm.pressed? Wii::BUTTON_RIGHT
             puts "RIGHT!"
         elsif wm.pressed? Wii::BUTTON_PLUS
-            puts "+!"
+            accel_on(wm)
         elsif wm.pressed? Wii::BUTTON_MINUS
-            puts "-!"
+            vres(wm)
         elsif wm.pressed? Wii::BUTTON_HOME
-            puts "HOME!"
+            sens_pos(wm)
         elsif wm.pressed? Wii::BUTTON_ONE
-            puts "1!"
+            a_ratio(wm)
         elsif wm.pressed? Wii::BUTTON_TWO
-            puts "2!"
+            sens(wm)
         end
       end
     end
@@ -39,3 +80,6 @@ end
 ensure
     w.cleanup!
 end
+
+
+
